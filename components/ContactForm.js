bfdc0e7e645d3c80841e6399808a056b6f11f71d
Ross Keenan <ross.keenan@protonmail.com>
2021-09-26 17:07:50 -0500

    Did a little styling on the contact form.

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,7 +9,7 @@ export default function Example() {
 
           <div className="grid grid-cols-1 lg:grid-cols-3">
             {/* Contact information */}
-            <div className="relative overflow-hidden py-10 px-6 bg-indigo-700 sm:px-10 xl:p-12">
+            <div className="relative overflow-hidden py-10 px-6 bg-blue-light sm:px-10 xl:p-12">
               <div
                 className="absolute inset-0 pointer-events-none sm:hidden"
                 aria-hidden="true"
@@ -113,51 +113,10 @@ export default function Example() {
                 Contact information
               </h3>
               <p className="mt-6 text-base text-indigo-50 max-w-3xl">
-                Nullam risus blandit ac aliquam justo ipsum. Quam mauris
-                volutpat massa dictumst amet. Sapien tortor lacus arcu.
+                Please fill out this contact form, and let me know what I can
+                help you with.
               </p>
-              <dl className="mt-8 space-y-6">
-                <dt>
-                  <span className="sr-only">Phone number</span>
-                </dt>
-                <dd className="flex text-base text-indigo-50">
-                  <PhoneIcon
-                    className="flex-shrink-0 w-6 h-6 text-indigo-200"
-                    aria-hidden="true"
-                  />
-                  <span className="ml-3">+1 (555) 123-4567</span>
-                </dd>
-                <dt>
-                  <span className="sr-only">Email</span>
-                </dt>
-                <dd className="flex text-base text-indigo-50">
-                  <MailIcon
-                    className="flex-shrink-0 w-6 h-6 text-indigo-200"
-                    aria-hidden="true"
-                  />
-                  <span className="ml-3">support@workcation.com</span>
-                </dd>
-              </dl>
-              <ul role="list" className="mt-8 flex space-x-12">
-                <li>
-                  <a className="text-indigo-200 hover:text-indigo-100" href="#">
-                    <span className="sr-only">Facebook</span>
-                    <svg
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6"
-                      aria-hidden="true"
-                    >
-                      <path
-                        d="M22.258 1H2.242C1.556 1 1 1.556 1 2.242v20.016c0 .686.556 1.242 1.242 1.242h10.776v-8.713h-2.932V11.39h2.932V8.887c0-2.906 1.775-4.489 4.367-4.489 1.242 0 2.31.093 2.62.134v3.037l-1.797.001c-1.41 0-1.683.67-1.683 1.653v2.168h3.362l-.438 3.396h-2.924V23.5h5.733c.686 0 1.242-.556 1.242-1.242V2.242C23.5 1.556 22.944 1 22.258 1"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </a>
-                </li>
+              <ul role="list" className="mt-8 flex space-x-12 ">
                 <li>
                   <a className="text-indigo-200 hover:text-indigo-100" href="#">
                     <span className="sr-only">GitHub</span>
