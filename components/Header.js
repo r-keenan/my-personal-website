757fc5d0b81df3fa803e8f6fb2afabedda915128
Ross Keenan <ross.keenan@protonmail.com>
2021-10-02 10:34:37 -0500

    Removed boilerplate logo and image and replaced with my name.

--- components/Header.js
+++ components/Header.js
@@ -39,16 +39,7 @@ export default function Header(props) {
                 </div>
                 <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex-shrink-0 flex items-center">
-                    <img
-                      className="block lg:hidden h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                      alt="Workflow"
-                    />
-                    <img
-                      className="hidden lg:block h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                      alt="Workflow"
-                    />
+                    <h1>Ross Keenan</h1>
                   </div>
                   <div className="hidden sm:block sm:ml-6">
                     <div className="flex space-x-4">
