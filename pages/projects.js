c62e0155a35c764748299f52da5a8bb12190179c
Ross Keenan <ross.keenan@protonmail.com>
2021-10-02 18:34:26 -0500

    Added Archive link for old website.

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,10 @@
 export default function Projects() {
   return (
-    <div>
+    <div className="mt-20">
       <h1>Projects</h1>
+      <div>
+        <a href="http://archive.today/Lrrqp">My Old WordPress Site</a>
+      </div>
     </div>
   );
 }
