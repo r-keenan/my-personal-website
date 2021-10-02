18b13525bc69fc97fb841b21e117a1c5ab58f009
Ross Keenan <ross.keenan@protonmail.com>
2021-10-02 16:34:21 -0500

    Added in a font only for the h1 of my name on header.

--- components/Header.js
+++ components/Header.js
@@ -39,7 +39,7 @@ export default function Header(props) {
                 </div>
                 <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex-shrink-0 flex items-center">
-                    <h1>Ross Keenan</h1>
+                    <h1 id="myNameHeader">Ross Keenan</h1>
                   </div>
                   <div className="hidden sm:block sm:ml-6">
                     <div className="flex space-x-4">
