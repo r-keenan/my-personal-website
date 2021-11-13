844d758a8584349387fc3c21e3258e04c42f8c0d
Ross Keenan <ross.keenan@protonmail.com>
2021-11-13 14:51:37 -0600

    Removed unused section with quote and job position.

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -20,7 +20,7 @@ export default function AboutMe() {
           >
             <div className="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72" />
             <svg
-              className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
+              className="absolute top-8 left-1/2 -ml-3 lg:-right-1 lg:left-auto lg:top-12"
               width={404}
               height={392}
               fill="none"
@@ -63,31 +63,6 @@ export default function AboutMe() {
               />
               <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
               <div className="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90" />
-              <div className="relative px-8">
-                <blockquote className="mt-8">
-                  <div className="relative text-lg font-medium text-white md:flex-grow">
-                    <svg
-                      className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-400"
-                      fill="currentColor"
-                      viewBox="0 0 32 32"
-                      aria-hidden="true"
-                    >
-                      <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                    </svg>
-                    <p className="relative">
-                      Tincidunt integer commodo, cursus etiam aliquam neque, et.
-                      Consectetur pretium in volutpat, diam. Montes, magna
-                      cursus nulla feugiat dignissim id lobortis amet.
-                    </p>
-                  </div>
-
-                  <footer className="mt-4">
-                    <p className="text-base font-semibold text-indigo-200">
-                      Sarah Williams, CEO at Workcation
-                    </p>
-                  </footer>
-                </blockquote>
-              </div>
             </div>
           </div>
         </div>
