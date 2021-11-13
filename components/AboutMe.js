a8256f1745722568696dee7cf7ece50debd78ac1
Ross Keenan <ross.keenan@protonmail.com>
2021-11-13 14:46:29 -0600

    Removed unused stats section.

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -124,23 +124,7 @@ export default function AboutMe() {
             </div>
           </div>
 
-          {/* Stats section */}
           <div className="mt-10">
-            <dl className="grid grid-cols-2 gap-x-4 gap-y-8">
-              {stats.map((stat) => (
-                <div
-                  key={stat.label}
-                  className="border-t-2 border-gray-100 pt-6"
-                >
-                  <dt className="text-base font-medium text-gray-500">
-                    {stat.label}
-                  </dt>
-                  <dd className="text-3xl font-extrabold tracking-tight text-gray-900">
-                    {stat.value}
-                  </dd>
-                </div>
-              ))}
-            </dl>
             <div className="mt-10">
               <Link href="/contact">
                 <a className="text-base font-medium text-indigo-600">
