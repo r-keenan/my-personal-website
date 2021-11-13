8fc5cc95ecf5bb23adee0ca8e65b558f4210e181
Ross Keenan <ross.keenan@protonmail.com>
2021-11-13 15:10:49 -0600

    Removed background design from photo.

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -19,50 +19,15 @@ export default function AboutMe() {
             className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen"
           >
             <div className="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72" />
-            <svg
-              className="absolute top-8 left-1/2 -ml-3 lg:-right-1 lg:left-auto lg:top-12"
-              width={404}
-              height={392}
-              fill="none"
-              viewBox="0 0 404 392"
-            >
-              <defs>
-                <pattern
-                  id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width={404}
-                height={392}
-                fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)"
-              />
-            </svg>
           </div>
-          <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
-            {/* Testimonial card*/}
-            <div className="relative py-1 rounded-2xl shadow-xl overflow-hidden">
+          <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-20 lg:max-w-none lg:py-20">
+            <div className="relative rounded-2xl shadow-xl overflow-hidden">
               <Image
                 className="absolute inset-0 h-full w-full object-cover"
                 src={aboutMePic2}
                 alt="Me"
                 priority
               />
-              <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
-              <div className="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90" />
             </div>
           </div>
         </div>
