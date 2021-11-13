d9001a2ad553aab5366631bd162f59fa1346d5e3
Ross Keenan <ross.keenan@protonmail.com>
2021-11-13 17:26:24 -0600

    Created new blog post. Will eventually break this out to Strapi and go the headless CMS route.

--- components/Blog.js
+++ components/Blog.js
@@ -2,13 +2,28 @@ import Avatar from "./Avatar";
 
 const posts = [
   {
+    title: "Find Me on LinkedIn",
+    href: "#",
+    category: { name: "Article", href: "#" },
+    description: "Connect with me on LinkedIn!",
+    date: "Nov 1, 2021",
+    datetime: "2021-11-17",
+    imageUrl:
+      "https://images.unsplash.com/photo-1585288766827-c62e98d70191?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    readingTime: "4 min",
+    author: {
+      name: "Ross Keenan",
+      href: "/about_me",
+    },
+  },
+  {
     title: "Find Me on Upwork",
     href: "#",
     category: { name: "Article", href: "#" },
     description:
       "Here is my Upwork profile if you want to hire me for freelancing or consulting.",
     date: "Nov 1, 2021",
-    datetime: "2021-11-01",
+    datetime: "2021-11-16",
     imageUrl:
       "https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1625&q=80",
     readingTime: "4 min",
@@ -24,7 +39,7 @@ const posts = [
     description:
       "Here is my GitHub. Please check it out to get the most update to information for what I am currently working on.",
     date: "Nov 1, 2021",
-    datetime: "2021-11-01",
+    datetime: "2021-11-15",
     imageUrl:
       "https://images.unsplash.com/photo-1566837945700-30057527ade0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
     readingTime: "4 min",
@@ -39,7 +54,7 @@ const posts = [
     category: { name: "Article", href: "#" },
     description: "Built with Next.js, Tailwind CSS, and Tailwind UI.",
     date: "Oct 31, 2021",
-    datetime: "2021-10-31",
+    datetime: "2021-11-14",
     imageUrl:
       "https://images.unsplash.com/photo-1483058712412-4245e9b90334?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
     readingTime: "6 min",
@@ -52,7 +67,7 @@ const posts = [
 
 export default function BlogList() {
   return (
-    <div className="relative bg-white pb-20 px-4 sm:px-6 sm:py-24 lg:pt-32 lg:px-8 lg:pb-72">
+    <div className="relative bg-white pb-20 px-4 sm:px-6 sm:py-24 lg:pt-32 lg:px-8 lg:pb-">
       <div className="absolute inset-0">
         <div className="bg-white h-1/3 sm:h-2/3" />
       </div>
