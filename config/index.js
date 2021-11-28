282a4219b44124699878f65546b6ae49f9413f23
Ross Keenan <ross.keenan@protonmail.com>
2021-11-28 15:23:47 -0600

    Add more base urls in to get rid of 500 error on Post on Vercel's app platform.

--- config/index.js
+++ config/index.js
@@ -1,2 +1,5 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://localhost:3000" ||
+  "https://my-personal-website-jade.vercel.app/" ||
+  "https://rosskeenan.com";
