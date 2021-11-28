9fc94fb3dd67e888fed5b0e1f0eaf7f602554b3c
Ross Keenan <ross.keenan@protonmail.com>
2021-11-28 17:04:36 -0600

    Updated config to use process.env.

--- config/index.js
+++ config/index.js
@@ -1,6 +1,2 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ||
-  "my-personal-website-jade.vercel.app" ||
-  "http://localhost:3000" ||
-  "https://my-personal-website-r-keenan.vercel.app" ||
-  "https://rosskeenan.com";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_API_URL: "https://my-personal-website-jade.vercel.app",
+  },
+};
