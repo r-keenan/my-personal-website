c54ba4f472d9b048105ae00e3b59095a44a47e6e
Ross Keenan <ross.keenan@protonmail.com>
2021-11-28 15:26:05 -0600

    Removed slash to see if that gets rid of 500 error.

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
   "http://localhost:3000" ||
-  "https://my-personal-website-jade.vercel.app/" ||
+  "https://my-personal-website-jade.vercel.app" ||
   "https://rosskeenan.com";
