b607f9a53af67452f259d594d1f3169aa3065f09
Ross Keenan <ross.keenan@protonmail.com>
2021-11-28 16:46:50 -0600

    Seeing if moving this above localhost will chang the 500 error.

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
+  "my-personal-website-jade.vercel.app" ||
   "http://localhost:3000" ||
   "https://my-personal-website-r-keenan.vercel.app" ||
   "https://rosskeenan.com";
