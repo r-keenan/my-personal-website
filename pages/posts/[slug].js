e1b91c8d5deb65ec4941946fda9daa53dadb9b40
Ross Keenan <ross.keenan@protonmail.com>
2021-12-05 15:08:05 -0600

    Fixed responsive layout. It will need a little more touch up.

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -3,6 +3,7 @@ import { API_URL } from "@/config/index";
 import { useRouter } from "next/router";
 import Breadcrumb from "@/components/Breadcrumb";
 import Image from "next/image";
+import Avatar from "@/components/Avatar";
 
 export default function BlogPost({ post }) {
   const router = useRouter();
@@ -14,10 +15,10 @@ export default function BlogPost({ post }) {
           aria-hidden="true"
         ></div>
       </div>
-      <div className="relative px-4 sm:px-6 lg:px-8 lg:pt-5">
+      <div className="relative px-14 mb-10 sm:px-6 sm:mb-20 md:mb-32 lg:px-8 lg:py-5 lg:mb-40">
         <Breadcrumb title={post.title} slug={post.slug} />
         <div className="text-lg max-w-prose mx-auto">
-          <div className="mt-10">
+          <div className="pt-24">
             <Image
               className="object-cover rounded-md"
               src={post.imageUrl}
@@ -35,10 +36,10 @@ export default function BlogPost({ post }) {
             </span>
           </h1>
         </div>
-        <div className="grid grid-cols-1 grid-rows-2 justify-items-center place-content-center">
-          <div>
-            <div className="grid grid-cols-1 place-content-center mt-10 prose prose-indigo prose-lg text-gray-500 w-4/5 mx-10 sm:mx-20 lg:mx-40">
-              <p className="justify-self-center">
+        <div className="flex">
+          <div className="flex-1 items-center justify-center">
+            <div className="flex mt-10 prose prose-indigo prose-lg text-gray-500 sm:mx-20 lg:mx-80">
+              <p>
                 Faucibus commodo massa rhoncus, volutpat.{" "}
                 <strong>Dignissim</strong> sed <strong>eget risus enim</strong>.
                 Mattis mauris semper sed amet vitae sed turpis id. Id dolor
@@ -47,55 +48,6 @@ export default function BlogPost({ post }) {
                 Dignissim sed eget risus enim.{" "}
                 <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis
                 id.
-              </p>
-              <p>
-                Quis semper vulputate aliquam venenatis egestas sagittis quisque
-                orci. Donec commodo sit viverra aliquam porttitor ultrices
-                gravida eu. Tincidunt leo, elementum mattis elementum ut nisl,
-                justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis.
-                Amet, duis sed elit interdum dignissim.
-              </p>
-              <h2>From beginner to expert in 30 days</h2>
-              <p>
-                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
-                consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
-                vitae interdum mauris enim, consequat vulputate nibh. Maecenas
-                pellentesque id sed tellus mauris, ultrices mauris. Tincidunt
-                enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-                turpis ipsum eu a sed convallis diam.
-              </p>
-              <blockquote>
-                <p>
-                  Sagittis scelerisque nulla cursus in enim consectetur quam.
-                  Dictum urna sed consectetur neque tristique pellentesque.
-                  Blandit amet, sed aenean erat arcu morbi.
-                </p>
-              </blockquote>
-              <p>
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-                risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-                Id dolor praesent donec est. Odio penatibus risus viverra tellus
-                varius sit neque erat velit.
-              </p>
-              <figure>
-                <figcaption>
-                  Sagittis scelerisque nulla cursus in enim consectetur quam.
-                </figcaption>
-              </figure>
-              <h2>Everything you need to get up and running</h2>
-              <p>
-                Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
-                . Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-                ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-                viverra ridiculus non molestie. Gravida quis fringilla amet eget
-                dui tempor dignissim. Facilisis auctor venenatis varius nunc,
-                congue erat ac. Cras fermentum convallis quam.
-              </p>
-              <p>
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-                risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-                Id dolor praesent donec est. Odio penatibus risus viverra tellus
-                varius sit neque erat velit.
               </p>
             </div>
             <div className="mt-10">
