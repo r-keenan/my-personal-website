41e6e0f1df6e04056fc677625738b31492fa0852
Ross Keenan <ross.keenan@protonmail.com>
2022-01-11 11:21:28 -0600

    Added in a image on the Thank You page to avoid having so much empty white space and to a bit a of character to the page.

--- pages/thank_you.js
+++ pages/thank_you.js
@@ -1,11 +1,23 @@
+import Image from "next/image";
+import thankYou from "@/public/images/thank_you.jpeg";
+
 export default function ThankYou() {
   return (
     <div className="bg-white mt-10">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:py-32">
-        <div className="my-60 sm:my-52 lg:my-72">
-          <p className="text-center text-gray-medium">
-            Thank you for filling out this form. I will be in contact with you
-            in the next day or two.
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:py-24 sm:px-6 lg:px-8 lg:py-20">
+        <div className="my-10 sm:my-20 lg:my-16">
+          <Image
+            className="object-cover rounded-md"
+            src={thankYou}
+            alt="Thank You"
+            priority={true}
+            width={657}
+            height={305}
+            layout="responsive"
+          />
+          <p className="text-center text-gray-medium mt-10">
+            Thank you for filling out the form. I will be in contact with you in
+            the next day or two.
           </p>
         </div>
       </div>
