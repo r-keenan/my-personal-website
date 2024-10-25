0dd45457579bfe0e836b0cb617584554c8ba6cca
r-keenan <ross.keenan@protonmail.com>
2024-10-25 13:14:33 -0500

    Removed pages from tailwind config, since I am using the app router now.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
       blue: {
